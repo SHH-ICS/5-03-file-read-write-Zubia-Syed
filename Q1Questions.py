1875788b7597ec30aa349f9394047977937a7ada
Zubia Syed <Zubia.syed@stu.ocsb.ca>
2024-12-06 09:49:20 -0500

    Update Q1Questions.py
    Python changes

--- Q1Questions.py
+++ Q1Questions.py
@@ -1,2 +1,10 @@
 # Create a program that will accept a multiple choice question, four answers, and the letter of correct answer. 
 # This will be six lines, and then store the questions in the file questions.txt.
+
+a = 'Vancouver'
+b = 'edmonton'
+c = 'Ottawa'
+d = 'Toronto'
+Print(a,b,c,d)
+Answer = input("Enter: a,b,c,d")
+               
